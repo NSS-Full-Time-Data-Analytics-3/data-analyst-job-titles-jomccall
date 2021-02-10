3fda3e677b84aa814344b3d5e288077a0b4969c7
jomccall <mccalljoanna@gmail.com>
2021-02-10 15:10:05 -0600

    finalpostreview

--- da_jobs.sql
+++ da_jobs.sql
@@ -30,7 +30,7 @@ FROM da_jobs
 WHERE location = 'TN'
 AND star_rating >4;
 
-/* #5. How many postings have rev count btw 500 and 1000?*/
+/* #5. How many postings have rev count btw 500 and 1000? 151*/
 SELECT COUNT(title) 
 FROM da_jobs
 WHERE review_count BETWEEN 500 AND 1000;
@@ -40,76 +40,89 @@ SELECT location AS "state", AVG(star_rating) AS "avg_rating"
 FROM da_jobs
 GROUP BY location;
 
-/* #7. Select and count unique job titles. (add or remove *COUNT* to show or count)*/
+-- You can also round:
+SELECT location AS state, ROUND(AVG(star_rating),3) AS avg_rating
+FROM da_jobs
+GROUP BY location
+ORDER BY avg_rating DESC;
+
+/* #7. Select and count unique job titles. (add or remove *COUNT* to show or count) 881*/
 SELECT COUNT(DISTINCT title)
 FROM da_jobs;
 
-/* #8. How many unique job titles for CA companies? */
+/* #8. How many unique job titles for CA companies? 230*/
 SELECT COUNT(DISTINCT title) AS "CA_UniqueTitles"
-FROM da_jobs;
+FROM da_jobs
+WHERE location = 'CA';
 
 /* #9. Find name of company and avg. star rating for all companies
 with more than 5,000 reviews from all locations. How many are there? */
 SELECT DISTINCT company, AVG(star_rating) AS "Company_AVGRating"
 FROM da_jobs
-WHERE review_count <5000
-GROUP BY company;
+WHERE review_count > 5000
+AND COMPANY IS NOT NULL
+GROUP BY company
+ORDER BY AVG(star_rating);
 
+-- check a match
 SELECT COUNT(DISTINCT company)
 FROM da_jobs
-WHERE review_count <5000;
+WHERE review_count > 5000;
 
 /* #10. Order the query from #9 from highest to lowest avg star rating.
-Which company has the highest rating?: There are 12 companies with average ratings of 5:
-Acq. Life Cycle MGMT, Alteryx, Assoc. of Un. for Research in Astronomy
-Beaver Dam.....*/
-SELECT DISTINCT company, AVG(star_rating) AS "Company_AVGRating"
+Which company has the highest rating?: American Express through Unilever have same ratings,
+just listed in alphabetical order.
+*/
+SELECT DISTINCT company, round(AVG(star_rating),8) AS "Company_AVGRating"
 FROM da_jobs
-WHERE review_count <5000
+WHERE review_count > 5000
+AND company IS NOT NULL
+AND star_rating IS NOT NULL
 GROUP BY company
-ORDER BY AVG(star_rating) DESC;
+ORDER BY round(AVG(star_rating),8) DESC;
 
 /* #11. Find all job titles containing word "Analyst" how many different titles are there?*/
 SELECT COUNT(DISTINCT title)
 FROM da_jobs
-WHERE title LIKE '%Analyst%';
+WHERE title ILIKE '%Analyst%';
+-- 774
 
 SELECT COUNT(DISTINCT title)
 FROM da_jobs
-WHERE title LIKE '%Analytics';
+WHERE title ILIKE '%Analytics';
+-- 94
 
 SELECT COUNT(DISTINCT title)
 FROM da_jobs
-WHERE title LIKE '%Analytics'
-AND title LIKE '%Analyst%';
+WHERE title ILIKE '%Analytics'
+OR title ILIKE '%Analyst%';
+-- 827
+
+SELECT COUNT(DISTINCT title)
+FROM da_jobs
+WHERE title ILIKE '%Analytics'
+AND title ILIKE '%Analyst%';
+-- 41
 
 /* #12. How many job titles do not contain either the word "Analyst" or the word "Analytics"?
 What word do these positions have in common? */
 SELECT DISTINCT title
 FROM da_jobs
-WHERE title NOT LIKE '%Analyst%'
-AND title NOT LIKE '%Analytics%';
--- these positions have analyst or analytics with varying degrees of capital letters
--- in common.. it seems like all listing have either
+WHERE title NOT ILIKE '%Analyst%'
+AND title NOT ILIKE '%Analytics%';
+-- There are four, most have tableau in the title/ are about viz
 
 -- BONUS: Which jobs requiring SQL are hard to fill
 -- Find # of jobs by industry that require SQL and have been
 -- posted more than 3 weeks.
 
-SELECT *
-FROM da_jobs
-LIMIT 20;
-
-SELECT DISTINCT domain
-FROM da_jobs
-WHERE domain IS NOT NULL;
-
-SELECT DISTINCT domain, title, skill, (AVG(days_since_posting)) AS weeks
+SELECT DISTINCT(domain) AS industry,
+	COUNT(title) AS HTFjobs,
+	MAX(skill) as skill,
+	(AVG(days_since_posting)/7) AS weeks
 FROM da_jobs
 WHERE domain IS NOT NULL
-GROUP BY domain, title, skill
-HAVING skill LIKE '%SQL%'
-AND (AVG(days_since_posting)) > 3
-ORDER BY weeks DESC;
-
--- Aero+Def, Auto, Banks + FinServices
+AND skill ILIKE '%SQL%'
+GROUP BY domain
+HAVING (AVG(days_since_posting)/7) > 3
+ORDER BY HTFjobs DESC;
